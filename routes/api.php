d057cfd28e83ea4b183c132931fbad404fe16fe5
hamza <faizzoubi10@gmail.com>
2022-06-09 11:27:39 +0300

    batool27

--- routes/api.php
+++ routes/api.php
@@ -81,30 +81,36 @@ Route::post('Privilladge/create' , [App\Http\Controllers\PrivilladgeController::
 ///
 //Route::post(  '/Customer/html_email/{name}/{code}/{email}/{title}', [App\Http\Controllers\CustomerController::class, 'html_email']);
 Route::post('/Customer/register' , [App\Http\Controllers\CustomerController::class , 'register']) ;
+//Route::get('/Product/Product_All' , [App\Http\Controllers\ProductController::class , 'Product_All']);
+Route::get('/Product_All' , [App\Http\Controllers\ProductController::class , 'Product_All']);
+Route::get('/Product_Allf' , [App\Http\Controllers\ProductController::class , 'Product_Allf']);
+Route::post('/P2' , [App\Http\Controllers\ProductController::class , 'store']);
+
 Route::post('/Customer/changepassword' , [App\Http\Controllers\CustomerController::class , 'changepassword']) ;
 Route::post('/Customer/login' , [App\Http\Controllers\CustomerController::class , 'login']) ;
 Route::post('/Customer/logout' , [App\Http\Controllers\CustomerController::class , 'logout']) ;
+Route::get('/Product/Product_Order_sales' , [App\Http\Controllers\ProductController::class , 'Product_Order_sales']);
+Route::get('/Product/Product_Order_discount' , [App\Http\Controllers\ProductController::class , 'Product_Order_discount']);
+Route::get('/Product/Product_Order_favorite' , [App\Http\Controllers\ProductController::class , 'Product_Order_favorite']);
+Route::get('/Product/Product_Order_Salary' , [App\Http\Controllers\ProductController::class , 'Product_Order_Salary']);
+Route::post('/FavoriteStore/Add_Favorite/{id}' , [App\Http\Controllers\FavoriteStoreController::class , 'Add_Favorite']);
+Route::post('/FavoriteProduct/Add_Favorite/{id}' , [App\Http\Controllers\FavoriteProductController::class , 'Add_Favorite']);
+Route::get('/Product/Show_Secondray' , [App\Http\Controllers\SecondrayClassificationController::class , 'Show_Secondray']);
+Route::get('/Product/ShowClassification2/{id}' , [App\Http\Controllers\SecondrayClassificationController::class , 'ShowClassification2']);
+Route::get('/FavoriteStore/Show_Favorite' , [App\Http\Controllers\FavoriteStoreController::class , 'Show_Favorite']);
+
+Route::get('/Show_p' , [App\Http\Controllers\SecondrayClassificationController::class , 'Show_p']);
+
 Route::group(['middleware' => ['auth:sanctum']],
     function () {
 
 
         Route::get('/Product/Show_Detalis/{id}' , [App\Http\Controllers\ProductController::class , 'Show_Detalis']);
-        Route::get('/Product/Product_Order_sales' , [App\Http\Controllers\ProductController::class , 'Product_Order_sales']);
-        Route::get('/Product/Product_Order_discount' , [App\Http\Controllers\ProductController::class , 'Product_Order_discount']);
-        Route::get('/Product/Product_Order_favorite' , [App\Http\Controllers\ProductController::class , 'Product_Order_favorite']);
-        Route::get('/Product/Product_Order_Salary' , [App\Http\Controllers\ProductController::class , 'Product_Order_Salary']);
 
-        Route::get('/Product/Product_All' , [App\Http\Controllers\ProductController::class , 'Product_All']);
-        Route::get('/Product/Show_Classification' , [App\Http\Controllers\ClassificationController::class , 'Show_Classification']);
-        Route::get('/Product/Show_Secondray' , [App\Http\Controllers\SecondrayClassificationController::class , 'Show_Secondray']);
         Route::post('/SecondrayClassification/ShowClassification/{id}/{title}' , [App\Http\Controllers\SecondrayClassificationController::class , 'shwoo']);
-        Route::get('/FavoriteProduct/Show_Favorite' , [App\Http\Controllers\FavoriteProductController::class , 'Show_Favorite']);
         Route::delete('/FavoriteProduct/Delete_Favorite/{id}' , [App\Http\Controllers\FavoriteProductController::class , 'Delete_Favorite']);
-        Route::post('/FavoriteStore/Add_Favorite/{id}' , [App\Http\Controllers\FavoriteStoreController::class , 'Add_Favorite']);
-        Route::get('/FavoriteStore/Show_Favorite' , [App\Http\Controllers\FavoriteStoreController::class , 'Show_Favorite']);
         Route::delete('/FavoriteStore/Delete_Favorite/{id}' , [App\Http\Controllers\FavoriteStoreController::class , 'Delete_Favorite']);
 
 
         ;});
-        Route::post('/FavoriteProduct/Add_Favorite/{id}' , [App\Http\Controllers\FavoriteProductController::class , 'Add_Favorite']);
-
+Route::get('/FavoriteProduct/Show_Favorite' , [App\Http\Controllers\FavoriteProductController::class , 'Show_Favorite']);
