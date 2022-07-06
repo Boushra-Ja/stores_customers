df13d3a63f0b47ac085bdceb31e13c2aee33a250
Bayan <itebayan@gmail.com>
2022-07-06 22:27:18 +0300

    banyan44eh

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,6 @@ use App\Http\Controllers\RatingStoreController;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
-header("Access-Control-Max-Age", "3600");
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header("Access-Control-Allow-Credentials", "true");
-
-
 //////////////////Boushra//////////////////////////////
 ////Route Of Stores
 Route::resource('stores' , StoreController :: class) ;
