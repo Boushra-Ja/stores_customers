248a54ab73f3fe5a102489e86be5847758cd74f7
Bayan <itebayan@gmail.com>
2022-07-10 21:08:11 +0300

    banyan44eh

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -117,7 +117,7 @@ class CollectionController extends BaseController
     // تعديل مجموعة
     public function update(Request $request)
     {
-        $collection = Collection::find($request->id)->update($request->all());
+        $collection = Collection::where('id','=',$request->id)->update($request->all());
         return $this->sendResponse($collection, 'تم تعديل المجموعة بنجاح');
     }
 
